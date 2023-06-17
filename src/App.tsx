27b29df10d701610b425631e0ddf3d13d44e2bbb
Cactus <ysjungman@gmail.com>
2023-06-17 13:45:41 +0900

    437_RnT-props

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import Todos from './components/Todos';
 function App() {
   return (
     <div>
-      <Todos />
+      <Todos items={
+        [
+          'Learn React',
+          'Learn TypeScript',
+        ]} />
     </div>
   );
 }
