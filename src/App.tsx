b5894b2a39abfdaead21827a683cfe37aff486b9
Cactus <ysjungman@gmail.com>
2023-06-17 18:34:58 +0900

    443_Rnt-input추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { useState } from 'react';
+
 import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
 function App() {
-  const todos = [new Todo('Learn React😀'), new Todo('Learn TypeScript😎')];
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
 
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
   };
 
   return (
